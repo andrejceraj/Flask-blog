7e4107728d12af60b535a2ff2b39b8a866a98d75
Andrej Ceraj <andrej.ceraj@gmail.com>
2019-09-04 13:32:25 +0200

    added pagination

--- app/routes.py
+++ app/routes.py
@@ -7,11 +7,13 @@ from werkzeug.security import check_password_hash
 from werkzeug.urls import url_parse
 from datetime import datetime
 
+
 @app.before_request
 def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -19,14 +21,24 @@ def before_request():
 def index():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(body= form.post.data, author=current_user)
+        post = Post(body=form.post.data, author=current_user)
         db.session.add(post)
         db.session.commit()
         flash('Message successfully posted!')
         return redirect(url_for('index'))
 
-    posts = current_user.followed_posts().all()
-    return render_template('index.html', title='Home', posts=posts, form=form)
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(page, app.config['POSTS_PER_PAGE'], False)
+    next_url = None
+    prev_url = None
+    if posts.has_next:
+        next_url = url_for('index', page=posts.next_num)
+    if posts.has_prev:
+        prev_url = url_for('index', page=posts.prev_num)
+
+    return render_template('index.html', title='Home', posts=posts.items,
+                           form=form, next_url=next_url, prev_url=prev_url)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -46,10 +58,12 @@ def login():
         return redirect(next_page)
     return render_template('login.html', form=form, title='Sign in')
 
+
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('login'))
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -67,12 +81,21 @@ def register():
         return redirect(url_for('index'))
     return render_template('register.html', form=form)
 
+
 @app.route('/user/<username>')
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = user.posts
-    return render_template('user.html', user=user, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(page, app.config['POSTS_PER_PAGE'], False)
+    next_url = None
+    prev_url = None
+    if posts.has_next:
+        next_url = url_for('user', username=user.username, page=posts.next_num)
+    if posts.has_prev:
+        prev_url = url_for('user', username=user.username, page=posts.prev_num)
+    return render_template('user.html', user=user, posts=posts.items, next_url=next_url, prev_url=prev_url)
+
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
@@ -89,6 +112,7 @@ def edit_profile():
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', form=form)
 
+
 @app.route('/follow/<username>')
 @login_required
 def follow(username):
@@ -104,6 +128,7 @@ def follow(username):
     flash('You are following {}!'.format(username))
     return redirect(url_for('user', username=username))
 
+
 @app.route('/unfollow/<username>')
 @login_required
 def unfollow(username):
@@ -118,3 +143,17 @@ def unfollow(username):
     db.session.commit()
     flash('You are not following {}.'.format(username))
     return redirect(url_for('user', username=username))
+
+
+@app.route('/explore')
+@login_required
+def explore():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(page, app.config['POSTS_PER_PAGE'], False)
+    next_url = None
+    prev_url = None
+    if posts.has_next:
+        next_url = url_for('index', page=posts.next_num)
+    if posts.has_prev:
+        prev_url = url_for('index', page=posts.prev_num)
+    return render_template('index.html', title='Explore', posts=posts.items, next_url=next_url, prev_url=prev_url)
