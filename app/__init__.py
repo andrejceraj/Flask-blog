f433d5687b3c24f6b79e81c1e0bfcd43862ad227
Andrej Ceraj <andrej.ceraj@gmail.com>
2019-09-07 11:20:14 +0200

    improving app reusability
    added application factory, improved unit testing, written requirements

--- app/__init__.py
+++ app/__init__.py
@@ -10,36 +10,48 @@ from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-login = LoginManager(app)
+db = SQLAlchemy()
+migrate = Migrate()
+login = LoginManager()
 login.login_view = 'auth.login'
 login.login_message = 'Please log in to access this page.'
-mail = Mail(app)
-bootstrap = Bootstrap(app)
-moment = Moment(app)
+mail = Mail()
+bootstrap = Bootstrap()
+moment = Moment()
 
-from app.main import routes
 
-import logging
-from logging.handlers import RotatingFileHandler
-import os
-from app.errors import bp as error_bp
-from app.auth import bp as auth_bp
-from app.main import bp as main_bp
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
 
-app.register_blueprint(error_bp)
-app.register_blueprint(auth_bp, url_prefix='/auth')
-app.register_blueprint(main_bp)
+    db.init_app(app)
+    login.init_app(app)
+    migrate.init_app(app, db)
+    mail.init_app(app)
+    bootstrap.init_app(app)
+    moment.init_app(app)
 
-if not app.debug:
-    if not os.path.exists('logs'):
-        os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
-    file_handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
+    from app.main import routes
+    import logging
+    from logging.handlers import RotatingFileHandler
+    import os
+    from app.errors import bp as error_bp
+    from app.auth import bp as auth_bp
+    from app.main import bp as main_bp
 
-    app.logger.setLevel(logging.INFO)
-    app.logger.info('Microblog startup')
+    app.register_blueprint(error_bp)
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+    app.register_blueprint(main_bp)
+
+    if not app.debug:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Microblog startup')
+
+    return app
--- app/email.py
+++ app/email.py
@@ -1,11 +1,11 @@
+from flask import current_app
 from flask_mail import Message
 from app import mail
-from app import app
 from threading import Thread
 
 
-def send_async_mail(app, msg):
-    with app.app_context():
+def send_async_mail(current_app, msg):
+    with current_app.app_context():
         mail.send(msg)
 
 
@@ -13,4 +13,4 @@ def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    Thread(target=send_async_mail, args=(app, msg)).start()
+    Thread(target=send_async_mail, args=(current_app._get_current_object(), msg)).start()
